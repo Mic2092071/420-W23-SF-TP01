cfbe0c54a226c317d962af4eda47b38a41fee5f3
MichaelPerron <2092071@csfoy.ca>
2021-07-28 10:41:43 -0400

    Correction: Corriger erreurs d'execution script des foreign keys
    J'ai changer l'ordre de creation des tables qui est plus logique.
    Mit en commentaires les foreign keys et constraints qui devrait etre
    ajouté et executé en dernier dans le deuxieme script pour fonctionner.

--- Etape01_Etudiant01_Creation_Tables.sql
+++ Etape01_Etudiant01_Creation_Tables.sql
@@ -5,12 +5,14 @@ Marc-Antoine St-Hilaire
  
  Date  02-07-2021
 */
+-- -----------------------------------------------------
+-- Étape 1 : Création des tables                      --
+-- -----------------------------------------------------
 DROP DATABASE IF EXISTS PosteClientDFC;
 CREATE DATABASE IF NOT EXISTS PosteClientDFC;
 
 USE PosteClientDFC;
--- -----------------------------------------------------
--- Étape 1 : Création des tables                      --
+
 SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE TABLE logiciel(
@@ -28,32 +30,34 @@ CREATE TABLE editeur(
     nom_editeur VARCHAR(45) NOT NULL
 ) ENGINE = INNODB;
 
-CREATE TABLE poste_has_logiciel(
-    id_poste INT,
-    id_logiciel INT
-#     PRIMARY KEY(id_poste, id_logiciel),
-#     CONSTRAINT fk_poste_id_poste FOREIGN KEY (id_poste) REFERENCES poste(id_poste),
-#     CONSTRAINT fk_logiciel_id_logiciel FOREIGN KEY (id_logiciel) REFERENCES logiciel(id_logiciel)
-) ENGINE = INNODB;
-
 CREATE TABLE usager(
     id_usager INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nom_usager VARCHAR(45) NOT NULL,
     prenom_usager VARCHAR(45) NOT NULL,
     telephone_usager VARCHAR(15) NULL,
     local_id_local INT NULL
-#     FOREIGN KEY (local_id_local)
+# Aurait besoin d'être executer en dernier dans le 2e script.
+#     FOREIGN KEY(local_id_local)
 #         REFERENCES local(id_local)
 #         ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE = INNODB;
 
 CREATE TABLE usager_has_poste(
-    id_usager INT,
-    id_poste INT
-#     PRIMARY KEY(id_usager, id_poste),
-#     CONSTRAINT fk_usager_id_usager FOREIGN KEY (id_usager) REFERENCES usager(id_usager)
+    id_usager INT UNSIGNED,
+    id_poste INT UNSIGNED,
+    PRIMARY KEY (id_usager, id_poste),
+    CONSTRAINT fk_usager_id_usager FOREIGN KEY (id_usager) REFERENCES usager(id_usager)
+# Aurait besoin d'être executer en dernier dans le 2e script.
 #     CONSTRAINT fk_poste_id_poste FOREIGN KEY (id_poste) REFERENCES poste(id_poste)
 ) ENGINE = INNODB;
 
+CREATE TABLE poste_has_logiciel(
+    id_poste INT UNSIGNED,
+    id_logiciel INT UNSIGNED,
+    PRIMARY KEY(id_poste, id_logiciel),
+# Aurait besoin d'être executer en dernier dans le 2e script.
+#     CONSTRAINT fk_poste_id_poste FOREIGN KEY (id_poste) REFERENCES poste(id_poste),
+    CONSTRAINT fk_logiciel_id_logiciel FOREIGN KEY (id_logiciel) REFERENCES logiciel(id_logiciel)
+) ENGINE = INNODB;
+
 SET FOREIGN_KEY_CHECKS = 1;
--- -----------------------------------------------------
