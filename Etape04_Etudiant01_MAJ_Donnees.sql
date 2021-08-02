8a6a6cf2dd6985f10a369aed0232999ebe1a8ae6
MarcAStHilaire <86838045+MarcAStHilaire@users.noreply.github.com>
2021-08-02 11:09:25 -0400

    Étape 4 Etudiant01 Premiere partie

--- Etape04_Etudiant01_MAJ_Donnees.sql
+++ Etape04_Etudiant01_MAJ_Donnees.sql
@@ -10,3 +10,17 @@ USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
+
+UPDATE local 
+SET nb_bureau = '16', nb_prise_ethernet = '16', imprimante_id_peripherique = '674'
+WHERE nom_local = 'P-210';
+
+
+UPDATE local 
+SET nb_bureau = '27', nb_prise_ethernet = '27', imprimante_id_peripherique = '676'
+WHERE nom_local = 'P-308';
+
+UPDATE local 
+SET nb_bureau = '27', nb_prise_ethernet = '27', imprimante_id_peripherique = '677'
+WHERE nom_local = 'P-309';
+
