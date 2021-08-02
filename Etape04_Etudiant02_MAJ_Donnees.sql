34b5bc77f53950817cbd0765a62cd9a155dedace
MichaelPerron <2092071@csfoy.ca>
2021-08-02 19:29:04 -0400

    Ajout première version script MAJ Donnees

--- Etape04_Etudiant02_MAJ_Donnees.sql
+++ Etape04_Etudiant02_MAJ_Donnees.sql
@@ -1,12 +1,36 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
- 
- Date  JJ-MM-AAAA                                             
+Marc-Antoine St-Hilaire
+   --> Michael Perron
+
+ Date  02-08-2021
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
+
+UPDATE local
+SET nb_bureau = '27', nb_prise_ethernet = '27', imprimante_id_peripherique = '678'
+WHERE nom_local = 'P-409';
+
+UPDATE local
+SET nb_bureau = '5', nb_prise_ethernet = '6', imprimante_id_peripherique = '673'
+WHERE nom_local = 'P-416';
+
+UPDATE local
+SET nb_bureau = '27', nb_prise_ethernet = '27', imprimante_id_peripherique = '679'
+WHERE nom_local = 'P-418';
+
+UPDATE peripherique
+SET id_type_peripherique = '4' WHERE nom_peripherique LIKE 'Écran%';
+
+UPDATE peripherique
+SET id_type_peripherique = '1' WHERE nom_peripherique LIKE 'Imprimantes%';
+
+UPDATE peripherique
+SET id_type_peripherique = '2' WHERE nom_peripherique LIKE 'Souris%';
+
+UPDATE peripherique
+SET id_type_peripherique = '3' WHERE nom_peripherique LIKE 'Clavier%';
