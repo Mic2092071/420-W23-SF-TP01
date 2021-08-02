da474fa1d34644db207abb9700f1cebad86d7734
MarcAStHilaire <86838045+MarcAStHilaire@users.noreply.github.com>
2021-08-02 11:08:15 -0400

    Correction numéro d'étudiant

--- Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,48 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Marc-Antoine St-Hilaire
+Michael Perron
  
- Date  JJ-MM-AAAA                                             
+ Date  2021-08-02                                          
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS type_peripherique;
+CREATE TABLE type_peripherique
+(
+id_type_peripherique INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+nom_type_peripherique VARCHAR(25) NOT NULL
+)engine=InnoDb;
+
+
+INSERT INTO type_peripherique(nom_type_peripherique)
+VALUES 
+('Imprimantes'),
+('Souris'),
+('Clavier'),
+('Écran');
+
+
+ALTER TABLE local
+  ADD nb_bureau INT,
+  ADD nb_prise_ethernet INT,
+  ADD imprimante_id_peripherique INT,
+  ADD FOREIGN KEY (imprimante_id_peripherique) REFERENCES type_peripherique(id_type_peripherique)
+  ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+ALTER TABLE fabricant 
+  MODIFY nom_fabricant VARCHAR(45) NOT NULL,
+  ADD CONSTRAINT nom_fabricant UNIQUE (nom_fabricant);
+ 
+  
+ALTER TABLE local 
+ MODIFY nom_local VARCHAR(45) NOT NULL,
+ ADD CONSTRAINT nom_local UNIQUE (nom_local);
+
+  
