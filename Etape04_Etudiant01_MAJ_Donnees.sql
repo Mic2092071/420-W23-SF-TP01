c85525710c5481f7edb5dda43483f24d4fc59cf5
MarcAStHilaire <86838045+MarcAStHilaire@users.noreply.github.com>
2021-08-02 19:02:37 -0400

    Correction de quelques details.

--- Etape04_Etudiant01_MAJ_Donnees.sql
+++ Etape04_Etudiant01_MAJ_Donnees.sql
@@ -1,16 +1,16 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Marc-Antoine St-Hilaire
+ Michael Perron
  
- Date  JJ-MM-AAAA                                             
+ Date  02-08-2021                                         
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
-
+SET FOREIGN_KEY_CHECKS = 0;
 UPDATE local 
 SET nb_bureau = '16', nb_prise_ethernet = '16', imprimante_id_peripherique = '674'
 WHERE nom_local = 'P-210';
@@ -32,3 +32,4 @@ SET nom_statut = 'Employé' WHERE id_typeUsager >=100 AND id_typeUsager <=999;
 UPDATE typeusager 
 SET nom_statut = 'Étudiant' WHERE id_typeUsager >=1000;
 
+SET FOREIGN_KEY_CHECKS = 1;
