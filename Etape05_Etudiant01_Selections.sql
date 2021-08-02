534128ecc4c1c5d733540f8c2c712656ffd5915d
MarcAStHilaire <86838045+MarcAStHilaire@users.noreply.github.com>
2021-08-02 13:49:03 -0400

    Ajout Script Etape05 Etudiant01

--- Etape05_Etudiant01_Selections.sql
+++ Etape05_Etudiant01_Selections.sql
@@ -1,12 +1,22 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Marc-Antoine St-Hilaire
+ Michael Perron
  
- Date  JJ-MM-AAAA                                             
+ Date : 02-08-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 5 : Requêtes de sélections                   --
 -- -----------------------------------------------------
+
+SELECT nom_local AS 'No. local', nb_bureau AS 'Nb. bureau', nb_prise_ethernet AS 'Nb. prise ethernet', imprimante_id_peripherique AS 'No. Imprimante', nom_peripherique AS 'Nom de L''imprimante'
+FROM local
+INNER JOIN peripherique 
+ON id_peripherique = imprimante_id_peripherique;
+
+SELECT nom_logiciel AS 'Logiciel', nb_licence AS 'Nb. d''installations sur poste'
+FROM logiciel
+WHERE nb_licence != 0
+ORDER BY nb_licence DESC;
