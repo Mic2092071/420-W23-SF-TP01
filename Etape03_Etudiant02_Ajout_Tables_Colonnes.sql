cd2fc9e90f4ce1fd705b7acdcaea8059a1b7ba03
MichaelPerron <2092071@csfoy.ca>
2021-08-02 14:43:39 -0400

    Ajout premier script Ajout Tables Colonnes

--- Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
@@ -1,14 +1,35 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
+Marc-Antoine St-Hilaire
+   --> Michael Perron
  
- Date  JJ-MM-AAAA                                             
+ Date  02-08-2021
 */
-
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+SET FOREIGN_KEY_CHECKS = 0;
 
-  
+DROP TABLE IF EXISTS typeusager;
+CREATE TABLE IF NOT EXISTS typeusager(
+    id_typeUsager INT AUTO_INCREMENT PRIMARY KEY,
+    nom_statut VARCHAR(45)
+) ENGINE = INNODB;
+
+INSERT INTO typeUsager (nom_statut)
+VALUES ('Professeur'),
+       ('Employé'),
+       ('Étudiant');
+
+ALTER TABLE usager
+    ADD id_typeUsager INT NULL,
+    ADD FOREIGN KEY (id_typeUsager)
+        REFERENCES typeusager(id_typeUsager)
+        ON UPDATE CASCADE ON DELETE RESTRICT;
+
+ALTER TABLE logiciel
+MODIFY nom_logiciel VARCHAR(45) NOT NULL,
+ADD CONSTRAINT nom_logiciel UNIQUE (nom_logiciel);
+
+SET FOREIGN_KEY_CHECKS = 1;
